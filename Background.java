3b06d1df10fcdb79fbecf1dfb6bbbe5c19c21ca0
z-hanif <zainab.hanif02@gmail.com>
2023-04-04 16:21:51 -0400

    started on background and replaced some images

---
+++ Background.java
@@ -0,0 +1,81 @@
+import java.awt.Graphics2D;
+import java.awt.Dimension;
+import java.awt.Image;
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+
+public class Background {
+  	private Image bgImage;
+  	private int bgImageWidth;      		
+
+	private Dimension dimension;
+
+ 	private int bgX;
+	private int backgroundX;
+	private int backgroundX2;
+	private int bgDX;			// size of the background move (in pixels)
+
+
+	public Background(JFrame window, String imageFile, int bgDX) {
+
+    	this.bgImage = loadImage(imageFile);
+		dimension = window.getSize();
+    	bgImageWidth = dimension.width;	// get width of the background
+    		this.bgDX = bgDX;
+
+  	}
+
+
+  	public void moveRight() {
+
+		if (bgX == 0) {
+			backgroundX = 0;
+			backgroundX2 = bgImageWidth;			
+		}
+
+		bgX = bgX - bgDX;
+
+		backgroundX = backgroundX - bgDX;
+		backgroundX2 = backgroundX2 - bgDX;
+
+		if ((bgX + bgImageWidth) % bgImageWidth == 0) {
+			System.out.println ("Background change: bgX = " + bgX); 
+			backgroundX = 0;
+			backgroundX2 = bgImageWidth;
+		}
+
+
+  	}
+
+
+  	public void moveLeft() {
+	
+		if (bgX == 0) {
+			backgroundX = bgImageWidth * -1;
+			backgroundX2 = 0;			
+		}
+
+		bgX = bgX + bgDX;
+				
+		backgroundX = backgroundX + bgDX;	
+		backgroundX2 = backgroundX2 + bgDX;
+
+		if ((bgX + bgImageWidth) % bgImageWidth == 0) {
+			//System.out.println ("Background change: bgX = " + bgX); 
+			backgroundX = bgImageWidth * -1;
+			backgroundX2 = 0;
+		}			
+   	}
+ 
+
+  	public void draw (Graphics2D g2) {
+		g2.drawImage(bgImage, backgroundX, 0,  1920, 1080, null);
+		g2.drawImage(bgImage, backgroundX2, 0, 1920, 1080, null);
+  	}
+
+
+  	public Image loadImage (String fileName) {
+		return new ImageIcon(fileName).getImage();
+  	}
+
+}
---
+++ BackgroundManager.java
@@ -0,0 +1,63 @@
+/* BackgroundManager manages many backgrounds (wraparound images 
+   used for the game's background). 
+
+   Backgrounds 'further back' move slower than ones nearer the
+   foreground of the game, creating a parallax distance effect.
+
+   When a sprite is instructed to move left or right, the sprite
+   doesn't actually move, instead the backgrounds move in the 
+   opposite direction (right or left).
+
+*/
+
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+
+
+public class BackgroundManager {
+
+	private String bgImages[] = {"images/myimages/background/1sky.png",
+								"images/myimages/background/2cloud2.png",
+								"images/myimages/background/3mountain2.png",
+								"images/myimages/background/4mist.png",
+								"images/myimages/background/5hill.png",
+								"images/myimages/background/6forest.png",
+								"images/myimages/background/7foreground.png"};
+
+  	private int moveAmount[] = {0, 1, 2, 2, 3, 4, 8};  
+						// pixel amounts to move each background left or right
+
+  	private Background[] backgrounds;
+  	private int numBackgrounds;
+  	public BackgroundManager(JFrame window, int moveSize) {
+    		numBackgrounds = bgImages.length;
+    		backgrounds = new Background[numBackgrounds];
+
+    		for (int i = 0; i < numBackgrounds; i++) {
+       			backgrounds[i] = new Background(window, bgImages[i], moveAmount[i]);
+    		}
+  	} 
+
+
+  	public void moveRight() { 
+		for (int i=0; i < numBackgrounds; i++)
+      			backgrounds[i].moveRight();
+  	}
+
+
+  	public void moveLeft() {
+		for (int i=0; i < numBackgrounds; i++)
+      			backgrounds[i].moveLeft();
+  	}
+
+
+  	// The draw method draws the backgrounds on the screen. The
+  	// backgrounds are drawn from the back to the front.
+
+  	public void draw (Graphics2D g2) { 
+		for (int i=0; i < numBackgrounds; i++)
+      			backgrounds[i].draw(g2);
+  	}
+
+}
+
