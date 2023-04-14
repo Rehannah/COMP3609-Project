0c096d186e9732d11c8b52c46ab47fea8205d42b
Rehannah <rehannah12345@gmail.com>
2023-04-13 21:34:18 -0400

    level 2 background works

--- Level2Player.java
+++ Level2Player.java
@@ -27,10 +27,8 @@ public class Level2Player {
 	private HashMap<String, Animation> animations;
 
 
-	public Level2Player (JFrame window, BackgroundManager b) {
+	public Level2Player (JFrame window) {
 		this.window = window;
-
-		bgManager = b;			// instance of BackgroundManager
 
 		playerImage = ImageManager.loadImage("images/myimages/boy/Idle/1.png");	  
 		dimension = window.getSize();
@@ -49,17 +47,7 @@ public class Level2Player {
 		anim.addFrame(ImageManager.loadImage("images/myimages/boy/Run/4.png"), 175);
 		anim.addFrame(ImageManager.loadImage("images/myimages/boy/Run/5.png"), 125);
 		anim.addFrame(ImageManager.loadImage("images/myimages/boy/Run/6.png"), 150);
-		animations.put("run", anim);
-		
-		anim = new Animation(false);
-		anim.addFrame(ImageManager.loadImage("images/myimages/boy/Jump/1.png"), 100);
-		anim.addFrame(ImageManager.loadImage("images/myimages/boy/Jump/2.png"), 100);
-		anim.addFrame(ImageManager.loadImage("images/myimages/boy/Jump/3.png"), 100);
-		anim.addFrame(ImageManager.loadImage("images/myimages/boy/Jump/4.png"), 100);
-		anim.addFrame(ImageManager.loadImage("images/myimages/boy/Jump/5.png"), 100);
-		anim.addFrame(ImageManager.loadImage("images/myimages/boy/Jump/6.png"), 100);
-		animations.put("jump", anim);
-		
+		animations.put("run", anim);		
 	}
 
 	public int getXPos() {
@@ -68,6 +56,10 @@ public class Level2Player {
 
 	public int getYPos() {
 		return y;
+	}
+
+	public Image getImage() {
+		return playerImage;
 	}
 
 	public synchronized void move (int direction) {;
@@ -87,7 +79,14 @@ public class Level2Player {
 		}
 	}	
 
-	
+	public void draw(Graphics2D g2){
+		// // draw the background first
+		// bgManager.draw (g2);
+		// draw player
+
+		g2.drawImage(getImage(), x, y, null);
+	}
+  
 	public boolean collidesWithPlayer (int x, int y) {
 		Rectangle2D.Double myRectangle = getBoundingRectangle();
 		return myRectangle.contains(x, y);
